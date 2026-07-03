6aedf0ad5fac05cc1de89836cf83a8377f3ecc76
sruthi <sruthi735644@gmail.com>
2026-07-03 16:28:57 +0400

    'taskmanager'

--- backend/taskmanager/tasks/serializers.py
+++ backend/taskmanager/tasks/serializers.py
@@ -6,6 +6,7 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model=Task
         fields='__all__'
+        read_only_fields = ["user"]
 
 
 class RegisterSerializer(serializers.ModelSerializer):
